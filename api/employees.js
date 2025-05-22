9d7037e93b91bef3960b98f210b423b775a50bea
Danielle <dani.camp1125@gmail.com>
2025-05-22 17:44:37 -0400

    REST API

--- api/employees.js
+++ api/employees.js
@@ -1,12 +1,44 @@
 import express from "express";
 const router = express.Router();
-export default router;
+import employees from '#db/employeees.js'
 
 
 // TODO: this file!
 
+router.route("/")
+.get((req, res) => {
+    res.send(employees)
+})
+.post((req, res) => {
+    if(!req.body){
+        return res.status(400).send("Request is not found. Please send over an employee")
+    }
 
+const { employee } = req.body
 
+if(!employee){
+    return res.status(400).send("Please send a valid employee")
+}
 
+employees.push(employee)
+res.status(201).send(`Added the employee ${employee}`)
+})
 
-// }
+router.route("/random").get((req, res) => {
+    const randomIndex = Math.floor(Math.random() * employees.length);
+    res.send(employees[randomIndex]);
+  });
+  
+router.route("/:id").get((req, res) => {
+    const { id } = req.params;
+ 
+    const employee = employees.find((e) => e.id === +id);
+  
+    if (!employee) {
+      return res.status(404).send("Employee not found");
+    }
+  
+    res.send(employee);
+  });
+
+export default router
